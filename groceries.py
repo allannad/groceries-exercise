8c7756fcb93f7f25a2a0293e89aa670e5652ea0f
allannad <65513146+allannad@users.noreply.github.com>
2020-05-29 18:50:16 -0400

    update

--- groceries.py
+++ groceries.py
@@ -152,3 +152,60 @@ else:
 prods = [babies_num,beverages_num,dairyeggs_num,drygoodspasta_num,frozen_num,household_num,meatseafood_num,pantry_num,personalcare_num,snack_num]
 dept_prods = {key: value for key, value in zip(arr2, prods)}
 print('\n'.join("   + {} {}".format(k, v) for k, v in dept_prods.items()))
+
+
+
+
+
+
+
+
+#FROM CLASS
+#import operator
+#TODO: from sorted products variable get list of unique departments
+#if departmentname not in department
+products = [
+    {"id":1, "name": "Chocolate Sandwich Cookies", "department": "snacks", "aisle": "cookies cakes", "price": 3.50},
+    {"id":2, "name": "All-Seasons Salt", "department": "pantry", "aisle": "spices seasonings", "price": 4.99},
+    {"id":3, "name": "Robust Golden Unsweetened Oolong Tea", "department": "beverages", "aisle": "tea", "price": 2.49},
+    {"id":4, "name": "Smart Ones Classic Favorites Mini Rigatoni With Vodka Cream Sauce", "department": "frozen", "aisle": "frozen meals", "price": 6.99},
+    {"id":5, "name": "Green Chile Anytime Sauce", "department": "pantry", "aisle": "marinades meat preparation", "price": 7.99},
+    {"id":6, "name": "Dry Nose Oil", "department": "personal care", "aisle": "cold flu allergy", "price": 21.99},
+    {"id":7, "name": "Pure Coconut Water With Orange", "department": "beverages", "aisle": "juice nectars", "price": 3.50},
+    {"id":8, "name": "Cut Russet Potatoes Steam N' Mash", "department": "frozen", "aisle": "frozen produce", "price": 4.25},
+    {"id":9, "name": "Light Strawberry Blueberry Yogurt", "department": "dairy eggs", "aisle": "yogurt", "price": 6.50},
+    {"id":10, "name": "Sparkling Orange Juice & Prickly Pear Beverage", "department": "beverages", "aisle": "water seltzer sparkling water", "price": 2.99},
+    {"id":11, "name": "Peach Mango Juice", "department": "beverages", "aisle": "refrigerated", "price": 1.99},
+    {"id":12, "name": "Chocolate Fudge Layer Cake", "department": "frozen", "aisle": "frozen dessert", "price": 18.50},
+    {"id":13, "name": "Saline Nasal Mist", "department": "personal care", "aisle": "cold flu allergy", "price": 16.00},
+    {"id":14, "name": "Fresh Scent Dishwasher Cleaner", "department": "household", "aisle": "dish detergents", "price": 4.99},
+    {"id":15, "name": "Overnight Diapers Size 6", "department": "babies", "aisle": "diapers wipes", "price": 25.50},
+    {"id":16, "name": "Mint Chocolate Flavored Syrup", "department": "snacks", "aisle": "ice cream toppings", "price": 4.50},
+    {"id":17, "name": "Rendered Duck Fat", "department": "meat seafood", "aisle": "poultry counter", "price": 9.99},
+    {"id":18, "name": "Pizza for One Suprema Frozen Pizza", "department": "frozen", "aisle": "frozen pizza", "price": 12.50},
+    {"id":19, "name": "Gluten Free Quinoa Three Cheese & Mushroom Blend", "department": "dry goods pasta", "aisle": "grains rice dried goods", "price": 3.99},
+    {"id":20, "name": "Pomegranate Cranberry & Aloe Vera Enrich Drink", "department": "beverages", "aisle": "juice nectars", "price": 4.25}
+] 
+
+departments = []
+
+for x in products:
+    if x["department"] not in departments:
+        departments.append(x["department"])
+
+#matching_products = [x for x in products if x["department"] == departments]
+
+example = "snacks"
+matching_products = len([x["name"] for x in products if x["department"] == example])
+print(matching_products)
+
+for dept_name in departments:
+    matching_products = [x for x in products if x["department"] == dept_name]
+    matching_products_count = len(matching_products)
+    print(dept_name.title(), matching_products_count, "product(s)")
+
+#for x in products:
+#    price_usd = x["price"]
+#    print(f" + {x['name']} {price_usd}")
+#sorted_products = sorted(products, key=operator.itemgetter("name"))
+#PRINTING DEPARTMENTS
